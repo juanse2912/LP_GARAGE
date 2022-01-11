e2bee9f408f9dfc371b604398e2c97e026893f76
Daniel Alvarez Torres <dalvarezt@outlook.com>
2022-01-10 23:46:10 -0500

    Initial version of brakes

--- lp-garage-ui/model/brakes.js
+++ lp-garage-ui/model/brakes.js
@@ -1,8 +1,6 @@
 const math = require("./part").math;
 const Part = require("./part").Part;
 
-const SUBPARTS = new Map();
-SUBPARTS.set("BrakesAxles",BrakesAxles);
 
 class Brakes extends Part {
 
@@ -13,7 +11,6 @@ class Brakes extends Part {
         brakesGeneralProperties.inputParameters.frontAxleWeightLeveled.value = frontAxleWeightLeveled
         brakesGeneralProperties.inputParameters.rearAxleElevationHeight.value = rearAxleElevationHeight
         brakesGeneralProperties.inputParameters.frontAxleWeightWithRearAxleElevated.value = frontAxleWeightWithRearAxleElevated
-        brakesGeneralProperties.inputParameters.frontAxleWeigh.value = frontAxleWeigh;
         brakesGeneralProperties.inputParameters.rearAxleWeight.value = rearAxleWeight
         brakesGeneralProperties.inputParameters.brakePumpPistonDiameter.value = brakePumpPistonDiameter
         brakesGeneralProperties.inputParameters.numberOfCaliperPiston.value = numberOfCaliperPiston
@@ -27,38 +24,15 @@ class Brakes extends Part {
     
     static fromJSON(vehicle, j) {
         let brakes = new Brakes(vehicle, j.wheelbase, j.trackWidth, j.compressionRatio, j.frontAxleWeightLeveled, j.rearAxleElevationHeight, j.frontAxleWeightWithRearAxleElevated, j.frontAxleWeigh, j.rearAxleWeight, j.brakePumpPistonDiameter, j.numberOfCaliperPiston, j.caliperPistonDiameter, j.padFrictionCoefficient, j.padWidth, j.speedBeforeBraking, j.actualDistanceOfBraking);
-        for( let [partName, classH] of SUBPARTS) {
-            if(j[partName]) {
-                brakes.parts[partName] = classH.fromJSON(brakes, j[partName]);
-            }
-        }
-        return brakes;
+
     }
 
     toJSON(includeSubparts) {
         let result = super.toJSON();
-        result["intakeType"] = this.intakeType;
-        if (includeSubparts) {
-            for (let partName in this.parts) {
-                result[partName] = this.parts[partName].toJSON();
-            }
-        }
         return result;
     }
 
-    addPart(partName, j) {
-        let partClass = SUBPARTS.get(partName);
-        if (partClass) {
-            this.parts[partName] = partClass.fromJSON(this, j);
-        } else {
-            throw new Error("Invalid part name: " + partName);
-        }
-    }
 
-    getPart(partName) {
-        return this.parts[partName];
-    }
-    
 }
 
 module.exports.Brakes = Brakes;
